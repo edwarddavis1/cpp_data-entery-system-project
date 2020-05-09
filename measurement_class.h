101ed24abb598fa1979783dbf8248442d004c9e1
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2020-05-09 11:00:51 +0100

    Update measurement_class.h

--- measurement_class.h
+++ measurement_class.h
@@ -1,5 +1,10 @@
-// Header comments
-
+/*
+* Measurement Class Header
+* Abstract base class: measurement
+* Derived classes: nominal_data, double_data (variable objects)
+* Each variable class contains member functions for summary statistical measures
+* Last modified: 01/05/2020
+*/
 #ifndef MEASUREMENT_CLASS_H
 #define MEASUREMENT_CLASS_H
 
@@ -7,21 +12,18 @@ class measurement {
 public:
 	virtual ~measurement() {}
 	virtual void summary() const = 0;
-	virtual std::vector<std::string> summary_measures() const = 0;
-	virtual void print_data() = 0;
-	virtual int count() const = 0;
+	virtual std::vector<std::string> summary_measures() = 0; 
+	virtual void print_data() = 0;		
+	virtual int count() const = 0;							
 	virtual std::string get_variable_name() const = 0;
 	virtual std::vector<std::string> get_string_data() const = 0;
 	virtual std::string get_measurement_type() const = 0;
 	virtual std::vector<std::string> get_datestamps() const = 0;
 	virtual std::vector<double> get_error_data() const = 0;
+	virtual std::string mode_average() const = 0;						  
+	virtual int number_of_unique_values() const = 0;
 };
-class labelled_data : public measurement {
-public:
-	~labelled_data() {}
-	virtual std::string mode() = 0;
-};
-class nominal_data : public labelled_data {
+class nominal_data : public measurement {
 private:
 	std::vector<std::string> data;
 	std::string variable_name;
@@ -37,15 +39,14 @@ public:
 	std::vector<double> get_error_data() const;
 	std::string& operator[](size_t index);
 	void summary() const;
-	std::vector<std::string> summary_measures() const;
+	std::vector<std::string> summary_measures();
 	std::string get_measurement_type() const;
 	std::string get_variable_name();
 	int count() const;
-	std::string mode();
+	int number_of_unique_values() const;
+	std::string mode_average() const;
 	std::vector<std::string> get_string_data() const;
 	std::vector<std::string> get_data() const;
-	void add_data(std::vector<std::string> data_to_add);
-	void add_datestamps(std::vector<std::string> datestamps_to_add);
 };
 class double_data : public measurement {
 private:
@@ -56,7 +57,6 @@ private:
 public:
 	double_data() = default;
 	double_data(const std::string name_of_variable, const std::vector<double> variable_data, std::vector<std::string> variable_datestamps);
-	double_data(const std::string name_of_variable, const std::vector<double> variable_data, const double systematic_error, std::vector<std::string> variable_datestamps);
 	double_data(const std::string name_of_variable, const std::vector<double> variable_data, const std::vector<double> measurement_error, std::vector<std::string> variable_datestamps);
 	~double_data() {}
 	void print_data();
@@ -69,16 +69,16 @@ public:
 	std::vector<std::string> get_string_data() const;
 	std::vector<double> get_data() const;
 	int count() const;
+	int number_of_unique_values() const;
+	std::string mode_average() const;
 	double mean() const;
 	double standard_deviation() const;
+	double standard_error() const;
 	double min() const;
 	double max() const;
 	double get_percentile_value(double percentile) const;
 	void summary() const;
-	std::vector<std::string> summary_measures() const;
-	void add_data(std::vector<double> data_to_add);
-	void add_datestamps(std::vector<std::string> datestamps_to_add);
-	void add_errors(std::vector<double> errors_to_add);
+	std::vector<std::string> summary_measures();
 };
 
 #endif
