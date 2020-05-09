6a22b5548c1237967fb04518d43b8abb5823a521
edwarddavis1 <52565961+edwarddavis1@users.noreply.github.com>
2020-05-09 10:59:41 +0100

    Update data_management_system.cpp

--- data_management_system.cpp
+++ data_management_system.cpp
@@ -1,8 +1,12 @@
-// Edward Davis 9937240
-// Last modified: 
-// 
-// Compiled using Visual Studio 2017
-
+/* 
+* Edward Davis 9937240
+* Main script for the data management system, handling the running of the program and inputs
+* Program gives the user many different quick commands to explore available experiments
+* User can add to existing experiments or create new ones by either inputting data directly into the command line or through a csv file
+* Any edited experiments are contained for the duration that the program runs for and saved each experiment when quit
+* Last Modified: 01/05/2020
+* Compiled using Visual Studio 2019
+*/
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -34,37 +38,34 @@ int main()
 			<< "  delete ID:          deletes the selected experiment - this cannot be undone!\n"
 			<< "  saved experiments:  shows a list of available experiments with their ID number\n"
 			<< "  select ID:          selects an experiment to input data\n"
+			<< "  show edits:         displays experiments that have recently been edited\n"
 			<< "  new:                creates a new experiment to input data\n"
 			<< "  [filename.csv]      creates a new experiment from a csv file\n"
 			<< "  x:                  exits the program and saves edited experiments"
 			<< std::endl;
 		// Search for previous saved experiments
 		std::ifstream saved_experiments("list_of_experimentfiles.txt");
-
 		if (!saved_experiments.good()) {
 			std::cerr << "ERROR: File could not be opened" << std::endl;
-			std::cin.get();
-			return(1);
+			noexcept("list_of_experimentfiles.txt could not be found");
 		}
+		// Output list of available experiments with a file ID
 		std::string filename;
-		int file_number{ 0 };
+		int file_ID{ 0 };
 		std::vector<std::string> list_of_selections;
 		std::vector<std::string> list_of_available_experiments;
 		std::cout << "\nList of Available Experiments: " << std::endl;
 		while (getline(saved_experiments, filename))
 		{
-			file_number++;
-			list_of_selections.push_back(std::to_string(file_number));
+			file_ID++;
+			list_of_selections.push_back(std::to_string(file_ID));
 			list_of_available_experiments.push_back(filename);
-			std::cout << "\t(" << file_number << ") " << filename << std::endl;
+			std::cout << "\t(" << file_ID << ") " << filename << std::endl;
 		}
 		saved_experiments.close();
 		std::cout << std::endl;
-
-		// experiment object for the selected experiment object
+		// For each input quick command recognised, check file ID is valid and perform required action
 		experiment exp_for_input;
-
-		// initial inputs - for quick access to saved data
 		bool getting_input{ true };
 		while (getting_input)
 		{
@@ -72,7 +73,6 @@ int main()
 			getline(std::cin, input);
 			if (is_trigger_used(input, "print")) {
 				std::string selected_experiment_number{ get_selected_experiment_ID(input, "print") };
-				// check selection is valid
 				bool selection_valid{ check_selection_is_valid(selected_experiment_number, list_of_selections) };
 				if (selection_valid) {
 					int selected_experiment_number_int{ stoi(selected_experiment_number) };
@@ -84,7 +84,6 @@ int main()
 				} else std::cout << "I'm sorry, '" << selected_experiment_number << "' is not a valid selection" << std::endl;
 			} else if (is_trigger_used(input, "summarise")) {
 				std::string selected_experiment_number{ get_selected_experiment_ID(input, "summarise") };
-				// check selection is valid
 				bool selection_valid{ check_selection_is_valid(selected_experiment_number, list_of_selections) };
 				if (selection_valid) {
 					int selected_experiment_number_int{ stoi(selected_experiment_number) };
@@ -96,58 +95,54 @@ int main()
 				} else std::cout << "I'm sorry, '" << selected_experiment_number << "' is not a valid selection" << std::endl;
 			} else if (is_trigger_used(input, "delete")) {
 				std::string selected_experiment_number{ get_selected_experiment_ID(input, "delete") };
-				// check selection is valid
 				bool selection_valid{ check_selection_is_valid(selected_experiment_number, list_of_selections) };
 				if (selection_valid) {
 					int selected_experiment_number_int{ stoi(selected_experiment_number) };
 					int selected_experiment_index{ selected_experiment_number_int - 1 };
-
-					// delete experiment
+					// Delete experiment then update and output the new available experiments list
 					experiment selected_experiment(list_of_available_experiments[stoi(selected_experiment_number) - 1] + ".txt");
 					selected_experiment.delete_experiment();
-
-					// update the available experiments list
 					std::ifstream saved_experiments("list_of_experimentfiles.txt");
-
 					if (!saved_experiments.good()) {
 						std::cerr << "ERROR: File could not be opened" << std::endl;
+						noexcept("list_of_experimentfiles.txt could not be found");
 					} else {
-						file_number = 0;
+						file_ID = 0;
 						list_of_selections.clear();
 						list_of_available_experiments.clear();
 						while (getline(saved_experiments, filename)) {
-							file_number++;
-							list_of_selections.push_back(std::to_string(file_number));
+							file_ID++;
+							list_of_selections.push_back(std::to_string(file_ID));
 							list_of_available_experiments.push_back(filename);
-							std::cout << "\t(" << file_number << ") " << filename << std::endl;
+							std::cout << "\t(" << file_ID << ") " << filename << std::endl;
 						}
 						saved_experiments.close();
 						std::cout << std::endl;
 					}
 				} else std::cout << "I'm sorry, '" << selected_experiment_number << "' is not a valid selection" << std::endl;
 			} else if (input == "saved experiments") {
-				// Search for previous saved experiments
+				// Search for previous saved experiments and output found files with file ID
 				std::ifstream saved_experiments("list_of_experimentfiles.txt");
-
 				if (!saved_experiments.good()) {
 					std::cerr << "ERROR: File could not be opened" << std::endl;
+					noexcept("list_of_experimentfiles.txt could not be found");
 				} else {
-					file_number = 0;
+					file_ID = 0;
 					list_of_selections.clear();
 					list_of_available_experiments.clear();
 					while (getline(saved_experiments, filename))
 					{
-						file_number++;
-						list_of_selections.push_back(std::to_string(file_number));
+						file_ID++;
+						list_of_selections.push_back(std::to_string(file_ID));
 						list_of_available_experiments.push_back(filename);
-						std::cout << "\t(" << file_number << ") " << filename << std::endl;
+						std::cout << "\t(" << file_ID << ") " << filename << std::endl;
 					}
 					saved_experiments.close();
 					std::cout << std::endl;
 				}
 			} else if (is_trigger_used(input, "select")) {
+				// Select a file based on ID and create a corresponding experiment object for input
 				std::string selected_experiment_number{ get_selected_experiment_ID(input, "select") };
-				// check selection is valid
 				bool selection_valid{ check_selection_is_valid(selected_experiment_number, list_of_selections) };
 				if (selection_valid) {
 					int selected_experiment_number_int{ stoi(selected_experiment_number) };
@@ -158,6 +153,13 @@ int main()
 					getting_input = false;
 
 				} else std::cout << "I'm sorry, '" << selected_experiment_number << "' is not a valid selection" << std::endl;
+			} else if (input == "show edits") {
+				if (edited_experiments.size() == 0) std::cout << "No recently edited experiments" << std::endl;
+				// Output all experiments in the edited_experiments vector using a lambda function
+				for_each(edited_experiments.begin(), edited_experiments.end(), [](experiment exp)
+				{
+					std::cout << exp << std::endl;
+				});
 			} else if (input == "help") {
 				std::cout << "List of quick commands\n"
 					<< "  print ID:           returns a table showing the data from the selected experiment\n"
@@ -165,12 +167,13 @@ int main()
 					<< "  delete ID:          deletes the selected experiment - this cannot be undone!\n"
 					<< "  saved experiments:  shows a list of available experiments with their ID number\n"
 					<< "  select ID:          selects an experiment to input data\n"
+					<< "  show edits:         displays experiments that have recently been edited\n"
 					<< "  [filename.csv]      creates a new experiment from a csv file\n"
 					<< "  new:                creates a new experiment to input data\n"
 					<< "  x:                  exits the program and saves edited experiments"
 					<< std::endl;
 			} else if (input.find(".csv") != std::string::npos) {
-				// Create new experiment from csv file
+				// If a *.csv is input, create an object from that file
 				experiment exp_csv{ input };
 				std::cout << "File found, please name your experiment" << std::endl;
 				bool title_accepted{ false };
@@ -184,6 +187,7 @@ int main()
 						exp_csv.set_title(csv_exp_title);
 					}
 				}
+				// User can either save the *.csv as a new experiment or add to the experiment
 				std::cout << exp_csv << std::endl;
 				std::cout << "\nWould you like to [save] or [select] this file?" << std::endl;
 				bool csv_choice_accepted{ false };
@@ -202,17 +206,233 @@ int main()
 						std::cout << "ERROR: Please type either [save] or [select]" << std::endl;
 					}
 				}
-				if (save_csv) exp_csv.save_experiment();
+				if (save_csv) {
+					exp_csv.save_experiment();
+					edited_experiments.push_back(exp_csv);
+				}
 				else if (select_csv) {
 					exp_for_input = exp_csv;
 					getting_input = false;
 				}
+			} else if (input == "new") {
+				// Create a new experiment through manual input
+				experiment new_exp;
+				bool title_accepted{ false };
+				// Name the experiment
+				while (!title_accepted) {
+					std::cout << "Experiment title: ";
+					std::string exp_title;
+					getline(std::cin, exp_title);
+					if (exp_title.find("|") != std::string::npos) {
+						std::cout << "ERROR: Please do not use '|' in your title" << std::endl;
+					} else {
+						title_accepted = true;
+						new_exp.set_title(exp_title);
+					}
+				}
+				// Create variables
+				std::vector<std::string> new_variable_names;
+				bool adding_variables{ true };
+				int number_of_new_variables{ 0 };
+				std::cout << "\nEnter the names of your variables (type 'x' when done):" << std::endl;
+				while (adding_variables) {
+					// Name the variable
+					std::string variable_name;
+					bool variable_name_accepted{ false };
+					while (!variable_name_accepted) {
+						std::cout << "Variable name " << number_of_new_variables + 1 << " : ";
+						getline(std::cin, variable_name);
+						if (variable_name.find("|") != std::string::npos) {
+							std::cout << "ERROR: Please do not use '|' in your name" << std::endl;
+						} else if(variable_name == "x") {
+							adding_variables = false;
+							break;
+						} else {
+							variable_name_accepted = true;
+							new_variable_names.push_back(variable_name);
+							number_of_new_variables++;
+						}
+					}
+
+				}
+				// Enter the date
+				std::string datestamp;
+				bool datestamp_accepted{ false };
+				while (!datestamp_accepted) {
+					std::cout << "\nEnter the date (dd/mm/yyyy): ";
+					getline(std::cin, datestamp);
+					if (!is_data_point_a_datestamp(datestamp)) std::cout << "ERROR: Please enter a date in the format dd/mm/yyyy" << std::endl;
+					else datestamp_accepted = true;
+				}
+				// Enter variable data row by row
+				std::cout << "\nEnter the data row by row" << std::endl;
+				int row_number{ 1 };
+				std::vector<std::vector<std::string>> string_new_variable_data(new_variable_names.size());
+				bool getting_data{ true };
+				while (getting_data) {
+					for (size_t i{ 0 }; i < new_variable_names.size(); i++) {
+						bool data_point_accepted{ false };
+						while (!data_point_accepted) {
+							std::cout << new_variable_names[i] << "[" << row_number << "]: ";
+							std::string string_data_point;
+							getline(std::cin, string_data_point);
+							if (string_data_point.find("|") != std::string::npos) {
+								std::cout << "ERROR: Please do not use '|' in data point" << std::endl;
+							} else {
+								data_point_accepted = true;
+								string_new_variable_data[i].push_back(string_data_point);
+							}
+						}
+					}
+					std::cout << "Enter another row? [y/n]" << std::endl;
+					bool new_row_choice_accepted{ false };
+					while (!new_row_choice_accepted) {
+						std::string new_row_choice;
+						getline(std::cin, new_row_choice);
+						if (new_row_choice == "y") {
+							new_row_choice_accepted = true;
+						} else if (new_row_choice == "n") {
+							new_row_choice_accepted = true;
+							getting_data = false;
+						} else std::cout << "ERROR: Please enter 'y' or 'n'" << std::endl;
+					}
+					row_number++;
+				}
+				// Check if data is double or nominal for variable object creation
+				std::vector<std::string> variable_data_types;
+				size_t number_of_double_variables{ string_new_variable_data.size() };
+				for (size_t i{ 0 }; i < new_variable_names.size(); i++) {
+					bool found_string{ false };
+					for (size_t j{ 0 }; j < string_new_variable_data[0].size(); j++) {
+						if (!is_data_point_a_number(string_new_variable_data[i][j])) {
+							found_string = true;
+							break;
+						}
+					}
+					std::string type{ "double" };
+					if (found_string) {
+						type = "nominal";
+						number_of_double_variables--;
+					}
+					variable_data_types.push_back(type);
+				}
+				// Convert any numierical data from string to double
+				std::vector<std::vector<double>> double_new_variable_data(number_of_double_variables);
+				int index{ 0 };
+				for (size_t i{ 0 }; i < variable_data_types.size(); i++) {
+					if (variable_data_types[i] == "double") {
+						for (size_t j{ 0 }; j < string_new_variable_data[i].size(); j++) {
+							double_new_variable_data[index].push_back(stod(string_new_variable_data[i][j]));
+						}
+						index++;
+					}
+				}
+				// State variables with systematic errors
+				std::cout << "\nType the name of any variables with a systematic error, separated by commas" << std::endl;
+				bool accepted_variable_choices{ false };
+				std::vector<std::string> variables_with_systematic_errors;
+				while (!accepted_variable_choices) {
+					std::string input_list_of_variables;
+					getline(std::cin, input_list_of_variables);
+					// Count the number of selected variables
+					int number_of_choices{ 1 };
+					for (size_t i{ 0 }; i < input_list_of_variables.length(); i++) {
+						if (input_list_of_variables[i] == ',') number_of_choices++;
+					}
+					// Check, then get the selected variables
+					bool any_bad_choices{ false };
+					std::vector<std::string> temp_variables_with_systematic_errors;
+					for (int i{ 0 }; i < number_of_choices; i++) {
+						std::string choice{ choose_split(input_list_of_variables, ',', i) };
+						bool variable_okay{ false };
+						// Check variable exists
+						for (size_t j{ 0 }; j < new_variable_names.size(); j++) {
+							if (choice == new_variable_names[j]) {
+								// Check variable is a double
+								if (variable_data_types[j] == "double") variable_okay = true;
+							}
+						}
+
+						if (variable_okay)	temp_variables_with_systematic_errors.push_back(choice);
+						else {
+							std::cout << choice << " was not one of the variable names you entered, please try again" << std::endl;
+							any_bad_choices = true;
+							break;
+						}
+					}
+					if (!any_bad_choices) {
+						variables_with_systematic_errors = temp_variables_with_systematic_errors;
+						accepted_variable_choices = true;
+					}
+				}
+				// Enter systematic errors
+				std::vector<std::vector<double>> systematic_errors(variables_with_systematic_errors.size());
+				std::cout << "Enter the systematic error for each variable" << std::endl;
+				for (size_t j{ 0 }; j < string_new_variable_data[0].size(); j++) {
+					for (size_t i{ 0 }; i < variables_with_systematic_errors.size(); i++) {
+						bool error_accepted{ false };
+						while (!error_accepted) {
+							// Get the entered value
+							int variable_index{ 0 };
+							for (size_t k{ 0 }; k < new_variable_names.size(); k++) {
+								if (variables_with_systematic_errors[i] == new_variable_names[k]) variable_index = k;
+							}
+							std::cout << variables_with_systematic_errors[i] << "[" << j+1 << "] (value = " << string_new_variable_data[variable_index][j] << "): ";
+							std::string error;
+							getline(std::cin, error);
+							// Check error is numerical
+							if (is_data_point_a_number(error)) {
+								systematic_errors[i].push_back(stod(error));
+								error_accepted = true;
+							} else std::cout << "ERROR: Please enter a number" << std::endl;
+						}
+					}
+				}
+				// Create variable objects and add to experiment
+				int double_index{ 0 };
+				for (size_t i{ 0 }; i < variable_data_types.size(); i++) {
+					std::string type{ variable_data_types[i] };
+					if (type == "double") {
+						// Check if it has errors
+						std::vector<double> variable_errors;
+						bool has_errors{ false };
+						for (size_t j{ 0 }; j < variables_with_systematic_errors.size(); j++) {
+							if (new_variable_names[i] == variables_with_systematic_errors[j]) {
+								has_errors = true;
+								variable_errors = systematic_errors[j];
+							}
+						}
+						// If it has no errors, set all to zero
+						if (!has_errors) {
+							for (size_t j{ 0 }; j < string_new_variable_data[i].size(); j++) {
+								variable_errors.push_back(0);
+							}
+						}
+						// Create variable and add to experiment
+						double_data temp_variable{ new_variable_names[i], double_new_variable_data[double_index], variable_errors, datestamps_from_single_date(datestamp, double_new_variable_data[double_index].size())};
+						new_exp.add_variable(temp_variable);
+						double_index++;
+					} else if (type == "nominal") {
+						// Create variable and add to experiment
+						nominal_data temp_variable{new_variable_names[i], string_new_variable_data[i], datestamps_from_single_date(datestamp, string_new_variable_data[i].size()) };
+					}
+				}
+
+				// Show experiment
+				std::cout << "\nHere is your experiment\n" << std::endl;
+				std::cout << new_exp << std::endl;
+				new_exp.summary();
+
+				// Add to container
+				edited_experiments.push_back(new_exp);
+				
 			} else if (input == "x") {
 				getting_input = false;
 				program_running = false;
 				program_shutting_down = true;
 				break;
 			} else {
+				// If command not recognised, ask for another input
 				bool selection_valid{ false };
 				for (size_t i{ 0 }; i < list_of_selections.size(); i++)
 				{
@@ -224,7 +444,7 @@ int main()
 		}
 		if (program_shutting_down) break;
 
-		// input data for selected experiment
+		// Input data for selected experiment
 		std::cout << exp_for_input.get_title() << " so far..." << std::endl;
 		std::cout << exp_for_input << std::endl;
 		std::cout << "Inputting data for " << exp_for_input.get_title() << "..." << std::endl;
@@ -244,7 +464,6 @@ int main()
 			else if (variable_types_for_input[i] == "error") num_errors++;
 			else if (variable_types_for_input[i] == "datestamp") num_datestamps++;
 		}
-
 		// Containers for input data: vectors of variable data
 		std::vector<std::vector<double>> double_input_data(num_doubles);
 		std::vector < std::vector<double>> error_input_data(num_errors);
